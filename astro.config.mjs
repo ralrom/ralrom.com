3db36fb591a292a1b68592b298af48fb23eb7a93
Robert Al-Romhein <robert.alromhein@gmail.com>
2022-10-16 11:25:25 -0400

    Move to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,15 +10,8 @@ import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
-
-// https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), preact()],
-  output: "server",
-  adapter: netlify(),
   markdown: {
     shikiConfig: {
       wrap: true,
