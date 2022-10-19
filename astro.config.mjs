aa8ce630d826c1261454bd8312fad885cc2adf24
Robert Al-Romhein <robert.alromhein@gmail.com>
2022-10-19 08:31:57 -0400

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,17 @@ import m2dx from "astro-m2dx";
 import { defineConfig } from "astro/config";
 
 /** @type {import('astro-m2dx').Options} */
+import sitemap from "@astrojs/sitemap";
 const m2dxOptions = {
   frontmatter: true,
 };
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), preact()],
+  site: "https://www.ralrom.com",
+  integrations: [mdx(), tailwind(), preact(), sitemap()],
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [[m2dx, m2dxOptions]],
-    // shikiConfig: {
-    //   wrap: true,
-    // },
   },
 });
