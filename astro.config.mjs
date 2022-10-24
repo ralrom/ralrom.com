b7718565925d9c54a064bf99e5fda713b94c293f
Robert Al-Romhein <robert.alromhein@gmail.com>
2022-10-24 07:55:41 -0400

    Add Lucky Gorilla brochure post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,27 @@
+import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import m2dx from "astro-m2dx";
 import { defineConfig } from "astro/config";
 
 /** @type {import('astro-m2dx').Options} */
-import sitemap from "@astrojs/sitemap";
 const m2dxOptions = {
   frontmatter: true,
 };
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.ralrom.com",
-  integrations: [mdx(), tailwind(), preact(), sitemap()],
+  integrations: [
+    mdx(),
+    tailwind(),
+    preact(),
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
   markdown: {
     extendDefaultPlugins: true,
     remarkPlugins: [[m2dx, m2dxOptions]],
