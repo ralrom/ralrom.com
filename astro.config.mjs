ce32b707e3b065bc214f90c0a1eb8ecefc601310
Robert Al-Romhein <robert.alromhein@gmail.com>
2024-08-25 16:40:34 -0400

    remove old unused plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@ import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-// import m2dx from "astro-m2dx";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import icon from "astro-icon";
-
-// const m2dxOptions = {
-//   frontmatter: true,
-// };
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,9 +25,5 @@ export default defineConfig({
     preact(),
     sitemap(),
   ],
-  // markdown: {
-  // extendDefaultPlugins: true,
-  // remarkPlugins: [[m2dx, m2dxOptions]],
-  // },
   adapter: netlify(),
 });
