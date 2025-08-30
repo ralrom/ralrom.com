d1d03a7edd23fdfc1f5ac4b9e2d94316190fa040
Robert Al-Romhein <robert.alromhein@gmail.com>
2025-08-30 07:27:20 -0400

    Update tiktok link

--- src/links.ts
+++ src/links.ts
@@ -2,6 +2,6 @@ export const links = [
   { label: "Instagram", link: "https://www.instagram.com/ralro_m/", icon: "mdi:instagram" },
   { label: "YouTube", link: "https://www.youtube.com/@heyralrom", icon: "mdi:youtube" },
   { label: "Twitter / X", link: "https://twitter.com/heyralrom", icon: "mdi:twitter" },
-  { label: "TikTok", link: "https://www.tiktok.com/@ralrom", icon: "cib:tiktok" },
+  { label: "TikTok", link: "https://www.tiktok.com/@ralrom5", icon: "cib:tiktok" },
   //   { label: "Facebook", link: "https://www.facebook.com/profile.php?id=61550850339706", icon: "mdi:facebook" },
 ];
