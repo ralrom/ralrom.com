dc535dcea0e6ee61e45f5c214fa0484fdb4a53e9
Robert Al-Romhein <robert.alromhein@gmail.com>
2025-10-20 08:32:53 -0400

    Update links to artwork

--- src/nav.ts
+++ src/nav.ts
@@ -6,7 +6,7 @@ export interface NavItem {
 
 export const nav: NavItem[] = [
   { name: "Home", href: "/", exactMatch: true },
-  { name: "Artwork", href: "/artwork" },
+  { name: "Artwork", href: "/artwork/1" },
   { name: "Blog", href: "/blog" },
   // { name: "Book List", href: "/books" },
 ];
